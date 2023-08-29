52a24fdea8336744648a203fc49580166508cf12
rdarosa <rafael.rosa@engdb.com.br>
2023-08-29 11:31:34 -0300

    Variables and data types

--- class-01/app.py
+++ class-01/app.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+name = "Jonh" 
+age = "57"
+last_name = "Wick"
+
+print("My name is", name)
+print("I am " +  age + " years old")
+print("My last name is " + last_name)
+print(f"My name is {name}.")
+
+age = "58"
+
+print(f"My name is {name} {last_name} and I am {age} years old")
+
+# Primitive data types
+name = "Rafael" # String
+age = 40 # int
+price = 1.1 # float
+is_male = True # boolean
+is_female = False # boolean
