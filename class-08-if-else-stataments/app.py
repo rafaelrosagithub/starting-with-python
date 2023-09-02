a70b16c6fbce5edd69b2a87fe8e6b2d89e27cc88
Rafael <rafael_unis@hotmail.com>
2023-09-02 14:06:29 -0300

    Comparison operators

--- class-08-if-else-stataments/app.py
+++ class-08-if-else-stataments/app.py
@@ -1,1 +1,61 @@
 # if-else statements, logical operators and comparison
+print("Start...")
+
+thirsty = True
+hunger = False
+diet = False
+
+# Logical operators
+# if thirsty:
+#     print("Drink water")
+# else: 
+#     print("I'm not thirsty")
+
+# # Operator or
+# if thirsty or hunger:
+#     print("Operator or")
+# else: 
+#     print("Thirst and hunger are false")
+
+# # Operator and
+# if thirsty and hunger:
+#     print("Operator and")
+# else: 
+#     print("Both need be true")
+
+# elif
+# if thirsty and hunger:
+#     print("Operator and")
+# elif thirsty and not hunger: 
+#     if diet:
+#         print("Drink water")
+#     else:
+#         print("Only thirsty")
+# elif not thirsty and hunger: 
+#     print("Only hunger")
+# else:
+#     print("Thirst and hunger are false")
+
+# Comparison operators
+num1 = 10
+num2 = 20
+
+if num1 == num2:
+    print("Equal")
+
+if num1 != num2:
+    print("Different")
+
+if num1 > num2:
+    print("Bigger then")
+
+if num1 < num2:
+    print("Less then    ")
+
+if num1 >= num2:
+    print("Bigger or equal")
+
+if num1 <= num2:
+    print("Less or equal")
+
+print("End...")
