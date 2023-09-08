20f2033b27a2dc1f1f9305fb2b2dcf6beb6195ec
Rafael <rafael_unis@hotmail.com>
2023-09-08 19:47:14 -0300

    Newsreader in python

--- news-reader/app.py
+++ news-reader/app.py
@@ -1,5 +1,8 @@
 import imaplib
 import credentials
+from bs4 import BeautifulSoup
+import quopri
+import pyttsx3
 
 host = "imap.gmail.com"
 port = 993
@@ -8,4 +11,23 @@ password = credentials.password
 
 server = imaplib.IMAP4_SSL(host, port)
 server.login(user, password)
-server.select("Deschamps")
+server.select("Test")
+status, data = server.search(None, "(UNSEEN)")
+
+for num in data[0].split():
+    status, data = server.fetch(num, "(RFC822)")
+    email_msg = data[0][1]
+
+    soup = BeautifulSoup(markup=email_msg, features="lxml")
+    news = soup.find_all("td")[0].text
+
+    utf = quopri.decodestring(news)
+    text = utf.decode("utf-8")
+
+    engine = pyttsx3.init()
+    # voices = engine.getProperty("voices")
+    # engine.setProperty("voice", voices[0].id)
+    # engine.setProperty("rate", 250)
+    engine.say(text)
+    engine.runAndWait()
+
