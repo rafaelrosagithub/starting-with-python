8f9f8628ff38e756baeb486beabafdae89a57ede
Rafael <rafael_unis@hotmail.com>
2023-09-12 19:49:33 -0300

    Created initial file

---
+++ basic-python-course/app.py
@@ -0,0 +1,15 @@
+# Programming logic
+
+# Step 0 - Understand the challenge we want to solve
+
+# Step 1 - Run all files from the database folder (Sales Folder)
+
+# Step 2 - Import the sales databases
+
+# Step 3 - Treat/Compile the databases
+
+# Step 4 - Calculate the best-selling product (in quantity)
+
+# Step 5 - Calculate the product that earned the most (in revenue)
+
+# Step 6 - Calculate the store/city that sold the most (in revenue) - create a graph/dashboard
